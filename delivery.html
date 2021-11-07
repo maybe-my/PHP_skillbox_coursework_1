d4847b98577bf0c301e7a8d36e0e5d04ccb447f3
maybe-my <loading4512@gmail.com>
2021-11-07 17:45:09 +0200

    init git

---
+++ delivery.html
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>Доставка</title>
+
+  <meta name="description" content="Fashion - интернет-магазин">
+  <meta name="keywords" content="Fashion, интернет-магазин, одежда, аксессуары">
+
+  <meta name="theme-color" content="#393939">
+
+  <link rel="preload" href="fonts/opensans-400-normal.woff2" as="font">
+  <link rel="preload" href="fonts/roboto-400-normal.woff2" as="font">
+  <link rel="preload" href="fonts/roboto-700-normal.woff2" as="font">
+
+  <link rel="icon" href="img/favicon.png">
+  <link rel="stylesheet" href="css/style.min.css">
+
+  <script src="js/scripts.js" defer=""></script>
+</head>
+<body>
+<header class="page-header">
+  <a class="page-header__logo" href="#">
+    <img src="img/logo.svg" alt="Fashion">
+  </a>
+  <nav class="page-header__menu">
+    <ul class="main-menu main-menu--header">
+      <li>
+        <a class="main-menu__item" href="#">Главная</a>
+      </li>
+      <li>
+        <a class="main-menu__item" href="#">Новинки</a>
+      </li>
+      <li>
+        <a class="main-menu__item" href="index.html">Sale</a>
+      </li>
+      <li>
+        <a class="main-menu__item active">Доставка</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+<main class="page-delivery">
+  <h1 class="h h--1">Доставка</h1>
+  <p class="page-delivery__desc">
+    Способы доставки могут изменяться в зависимости от адреса доставки, времени осуществления покупки и наличия товаров.
+  </p>
+  <p class="page-delivery__desc page-delivery__desc--strong">
+    <b>При оформлении покупки мы проинформируем вас о доступных способах доставки, стоимости и дате доставки вашего заказа.</b>
+  </p>
+  <section class="page-delivery__info">
+    <header class="page-delivery__desc">
+      Возможные варианты доставки:
+      <b class="page-delivery__variant">Доставка на дом:</b>
+    </header>
+    <ul class="page-delivery__list">
+      <li>
+        <b class="page-delivery__item-title">Стандартная доставка - 280 РУБ / БЕСПЛАТНО (ДЛЯ ЗАКАЗОВ ОТ 2000 РУБ)</b>
+        <p class="page-delivery__item-desc">
+          Примерный срок доставки составит около 2-7 рабочих дней, в зависимости от адреса доставки.
+        </p>
+      </li>
+      <li>
+        <b class="page-delivery__item-title">В день покупки - 560 РУБ</b>
+        <p class="page-delivery__item-desc">
+          Доступна для жителей г. Москва в пределах МКАД. Заказы, оформленныес понедельника по пятницу до 14:00 будут доставлены в тот же день с 19:00до 23:00. Изменение адреса доставки после оформления заказа невозможно.
+        </p>
+      </li>
+      <li>
+        <b class="page-delivery__item-title">Доставка с примеркой перед покупкой по Москве - 280 РУБ / БЕСПЛАТНО (ПРИ ВЫКУПЕ НА СУММУ ОТ 2000 РУБ)</b>
+        <p class="page-delivery__item-desc">
+          Доставка возможна только по Москве (в пределах МКАД) в течение 2-3 дней.
+          Воспользовавшись услугой «Примерка перед покупкой», вы можете получить свой заказ и примерить заказанные товары. Вы оплачиваете только то, что вам подошло. Максимальное количество позиций в заказе, при котором доступна примерка, составляет 10 вещей. Время на примерку одного заказа – 15 минут.
+        </p>
+      </li>
+    </ul>
+    <p class="page-delivery__desc">
+      Мы свяжемся с вами, чтобы подтвердить дату и время доставки. Кроме того, вы будете получать уведомления по электронной почте и SMS с информацией о номере заказа, его стоимости, а также с информацией о том, что заказ готов к выдаче. В день доставки заказа мы отправим вам SMS-уведомлениес номером телефона сотрудника службы доставки.
+    </p>
+    <a class="page-delivery__button button" href="index.html">Продолжить покупки</a>
+  </section>
+</main>
+<footer class="page-footer">
+  <div class="container">
+    <a class="page-footer__logo" href="#">
+      <img src="img/logo--footer.svg" alt="Fashion">
+    </a>
+    <nav class="page-footer__menu">
+      <ul class="main-menu main-menu--footer">
+        <li>
+          <a class="main-menu__item" href="#">Главная</a>
+        </li>
+        <li>
+          <a class="main-menu__item" href="#">Новинки</a>
+        </li>
+        <li>
+          <a class="main-menu__item" href="index.html">Sale</a>
+        </li>
+        <li>
+          <a class="main-menu__item">Доставка</a>
+        </li>
+      </ul>
+    </nav>
+    <address class="page-footer__copyright">
+      © Все права защищены
+    </address>
+  </div>
+</footer>
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,332 @@
+'use strict';
+
+const toggleHidden = (...fields) => {
+
+  fields.forEach((field) => {
+
+    if (field.hidden === true) {
+
+      field.hidden = false;
+
+    } else {
+
+      field.hidden = true;
+
+    }
+  });
+};
+
+const labelHidden = (form) => {
+
+  form.addEventListener('focusout', (evt) => {
+
+    const field = evt.target;
+    const label = field.nextElementSibling;
+
+    if (field.tagName === 'INPUT' && field.value && label) {
+
+      label.hidden = true;
+
+    } else if (label) {
+
+      label.hidden = false;
+
+    }
+  });
+};
+
+const toggleDelivery = (elem) => {
+
+  const delivery = elem.querySelector('.js-radio');
+  const deliveryYes = elem.querySelector('.shop-page__delivery--yes');
+  const deliveryNo = elem.querySelector('.shop-page__delivery--no');
+  const fields = deliveryYes.querySelectorAll('.custom-form__input');
+
+  delivery.addEventListener('change', (evt) => {
+
+    if (evt.target.id === 'dev-no') {
+
+      fields.forEach(inp => {
+        if (inp.required === true) {
+          inp.required = false;
+        }
+      });
+
+
+      toggleHidden(deliveryYes, deliveryNo);
+
+      deliveryNo.classList.add('fade');
+      setTimeout(() => {
+        deliveryNo.classList.remove('fade');
+      }, 1000);
+
+    } else {
+
+      fields.forEach(inp => {
+        if (inp.required === false) {
+          inp.required = true;
+        }
+      });
+
+      toggleHidden(deliveryYes, deliveryNo);
+
+      deliveryYes.classList.add('fade');
+      setTimeout(() => {
+        deliveryYes.classList.remove('fade');
+      }, 1000);
+    }
+  });
+};
+
+const filterWrapper = document.querySelector('.filter__list');
+if (filterWrapper) {
+
+  filterWrapper.addEventListener('click', evt => {
+
+    const filterList = filterWrapper.querySelectorAll('.filter__list-item');
+
+    filterList.forEach(filter => {
+
+      if (filter.classList.contains('active')) {
+
+        filter.classList.remove('active');
+
+      }
+
+    });
+
+    const filter = evt.target;
+
+    filter.classList.add('active');
+
+  });
+
+}
+
+const shopList = document.querySelector('.shop__list');
+if (shopList) {
+
+  shopList.addEventListener('click', (evt) => {
+
+    const prod = evt.path || (evt.composedPath && evt.composedPath());;
+
+    if (prod.some(pathItem => pathItem.classList && pathItem.classList.contains('shop__item'))) {
+
+      const shopOrder = document.querySelector('.shop-page__order');
+
+      toggleHidden(document.querySelector('.intro'), document.querySelector('.shop'), shopOrder);
+
+      window.scroll(0, 0);
+
+      shopOrder.classList.add('fade');
+      setTimeout(() => shopOrder.classList.remove('fade'), 1000);
+
+      const form = shopOrder.querySelector('.custom-form');
+      labelHidden(form);
+
+      toggleDelivery(shopOrder);
+
+      const buttonOrder = shopOrder.querySelector('.button');
+      const popupEnd = document.querySelector('.shop-page__popup-end');
+
+      buttonOrder.addEventListener('click', (evt) => {
+
+        form.noValidate = true;
+
+        const inputs = Array.from(shopOrder.querySelectorAll('[required]'));
+
+        inputs.forEach(inp => {
+
+          if (!!inp.value) {
+
+            if (inp.classList.contains('custom-form__input--error')) {
+              inp.classList.remove('custom-form__input--error');
+            }
+
+          } else {
+
+            inp.classList.add('custom-form__input--error');
+
+          }
+        });
+
+        if (inputs.every(inp => !!inp.value)) {
+
+          evt.preventDefault();
+
+          toggleHidden(shopOrder, popupEnd);
+
+          popupEnd.classList.add('fade');
+          setTimeout(() => popupEnd.classList.remove('fade'), 1000);
+
+          window.scroll(0, 0);
+
+          const buttonEnd = popupEnd.querySelector('.button');
+
+          buttonEnd.addEventListener('click', () => {
+
+
+            popupEnd.classList.add('fade-reverse');
+
+            setTimeout(() => {
+
+              popupEnd.classList.remove('fade-reverse');
+
+              toggleHidden(popupEnd, document.querySelector('.intro'), document.querySelector('.shop'));
+
+            }, 1000);
+
+          });
+
+        } else {
+          window.scroll(0, 0);
+          evt.preventDefault();
+        }
+      });
+    }
+  });
+}
+
+const pageOrderList = document.querySelector('.page-order__list');
+if (pageOrderList) {
+
+  pageOrderList.addEventListener('click', evt => {
+
+
+    if (evt.target.classList && evt.target.classList.contains('order-item__toggle')) {
+      var path = evt.path || (evt.composedPath && evt.composedPath());
+      Array.from(path).forEach(element => {
+
+        if (element.classList && element.classList.contains('page-order__item')) {
+
+          element.classList.toggle('order-item--active');
+
+        }
+
+      });
+
+      evt.target.classList.toggle('order-item__toggle--active');
+
+    }
+
+    if (evt.target.classList && evt.target.classList.contains('order-item__btn')) {
+
+      const status = evt.target.previousElementSibling;
+
+      if (status.classList && status.classList.contains('order-item__info--no')) {
+        status.textContent = 'Выполнено';
+      } else {
+        status.textContent = 'Не выполнено';
+      }
+
+      status.classList.toggle('order-item__info--no');
+      status.classList.toggle('order-item__info--yes');
+
+    }
+
+  });
+
+}
+
+const checkList = (list, btn) => {
+
+  if (list.children.length === 1) {
+
+    btn.hidden = false;
+
+  } else {
+    btn.hidden = true;
+  }
+
+};
+const addList = document.querySelector('.add-list');
+if (addList) {
+
+  const form = document.querySelector('.custom-form');
+  labelHidden(form);
+
+  const addButton = addList.querySelector('.add-list__item--add');
+  const addInput = addList.querySelector('#product-photo');
+
+  checkList(addList, addButton);
+
+  addInput.addEventListener('change', evt => {
+
+    const template = document.createElement('LI');
+    const img = document.createElement('IMG');
+
+    template.className = 'add-list__item add-list__item--active';
+    template.addEventListener('click', evt => {
+      addList.removeChild(evt.target);
+      addInput.value = '';
+      checkList(addList, addButton);
+    });
+
+    const file = evt.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (evt) => {
+      img.src = evt.target.result;
+      template.appendChild(img);
+      addList.appendChild(template);
+      checkList(addList, addButton);
+    };
+
+    reader.readAsDataURL(file);
+
+  });
+
+  const button = document.querySelector('.button');
+  const popupEnd = document.querySelector('.page-add__popup-end');
+
+  button.addEventListener('click', (evt) => {
+
+    evt.preventDefault();
+
+    form.hidden = true;
+    popupEnd.hidden = false;
+
+  })
+
+}
+
+const productsList = document.querySelector('.page-products__list');
+if (productsList) {
+
+  productsList.addEventListener('click', evt => {
+
+    const target = evt.target;
+
+    if (target.classList && target.classList.contains('product-item__delete')) {
+
+      productsList.removeChild(target.parentElement);
+
+    }
+
+  });
+
+}
+
+// jquery range maxmin
+if (document.querySelector('.shop-page')) {
+
+  $('.range__line').slider({
+    min: 350,
+    max: 32000,
+    values: [350, 32000],
+    range: true,
+    stop: function(event, ui) {
+
+      $('.min-price').text($('.range__line').slider('values', 0) + ' руб.');
+      $('.max-price').text($('.range__line').slider('values', 1) + ' руб.');
+
+    },
+    slide: function(event, ui) {
+
+      $('.min-price').text($('.range__line').slider('values', 0) + ' руб.');
+      $('.max-price').text($('.range__line').slider('values', 1) + ' руб.');
+
+    }
+  });
+
+}
